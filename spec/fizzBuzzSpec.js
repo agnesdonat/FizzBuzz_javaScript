f7c3d4f771f7b83a3b37dead67d5a8933d7addac
Vivian Allen <mrvivianallen@gmail.com>
2018-03-21 13:01:06 +0000

    buzz feature added, red->green

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -3,4 +3,8 @@ describe('FizzBuzz', function() {
     var threeNumber = Math.floor(Math.random(1) * 100 ) * 3;
     expect(fizzBuzz.play(threeNumber)).toEqual('Fizz');
   });
+  it('knows that multiples of 5 equals "Buzz"', function(){
+    var fiveNumber = Math.floor(Math.random(1) * 100) * 5;
+    expect(fizzBuzz.play(fiveNumber)).toEqual('Buzz');
+  });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,5 +4,8 @@ FizzBuzz.prototype.play = function(number) {
   if (number % 3 === 0) {
     return 'Fizz';
   }
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
 }
 let fizzBuzz = new FizzBuzz();
