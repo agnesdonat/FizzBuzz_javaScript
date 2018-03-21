ace04b45dda06b586b32e06cb997fff4aa6d1017
agnesdonat <agi.donat@gmail.com>
2018-03-21 13:19:24 +0000

    FizzBuzz finished

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,10 +1,23 @@
 describe('FizzBuzz', function() {
+
   it('knows that multiples of 3 equals "Fizz" ', function(){
     var threeNumber = Math.floor(Math.random(1) * 100 ) * 3;
     expect(fizzBuzz.play(threeNumber)).toEqual('Fizz');
   });
+
   it('knows that multiples of 5 equals "Buzz"', function(){
     var fiveNumber = Math.floor(Math.random(1) * 100) * 5;
     expect(fizzBuzz.play(fiveNumber)).toEqual('Buzz');
   });
+
+  it('knows the multiples of 15 equals "FizzBuzz"', function() {
+    var fifteenNumber = Math.floor(Math.random(1) * 100) * 15;
+    expect(fizzBuzz.play(fifteenNumber)).toEqual('FizzBuzz');
+  });
+
+  it('prints the number', function() {
+    var number = 4;
+    expect(fizzBuzz.play(number)).toEqual(number);
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,11 +1,20 @@
 function FizzBuzz() {}
 
 FizzBuzz.prototype.play = function(number) {
+  if (number % 15 === 0) {
+    return 'FizzBuzz';
+  }
   if (number % 3 === 0) {
     return 'Fizz';
   }
   if (number % 5 === 0) {
     return 'Buzz';
   }
+  return number;
+
 }
 let fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i<=100; i++) {
+  console.log(fizzBuzz.play(i));
+}
